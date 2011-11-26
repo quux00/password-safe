d29b89e3646555a9ba1eefcad4d6fb3e3345d6b2
Michael <mpeterson2@gmail.com>
2011-11-26 01:18:28 -0500

    Completed update, sort, delete.
    Not done thorough testing yet, but definitely close to having
    a finished product here.

--- pwsafe2.rb
+++ pwsafe2.rb
@@ -6,6 +6,11 @@ require 'gpgme'
 PASSWORD_SAFE_FILE = File.expand_path('~/.pwsafe2')
 DUMPFILE = "passwordsafe.plain.txt"  # default file for dumping contents to plaintext
 
+
+### -------------------------- ###
+### ---[ Class: SafeEntry ]--- ###
+### -------------------------- ###
+
 # Value object for a password safe entry
 # 
 # 
@@ -24,20 +29,14 @@ class SafeEntry
   # @option values [String] :url - optional
   # @option values [String] :notes - optional
   def initialize(values)
-    #DEBUG
-    require 'pp'
-    print "DEBUG 11: "
-    pp values.to_s
-    #END DEBUG
-    @name     = values[:name]     or raise RuntimeError, "no name provided for WebSite: #{values.inspect}"
-    @key      = values[:key]      || values[:name]  # shorthand key if desired
-    #DEBUG
-    puts "DEBUG 12: key just set: >>#{@key}<<"
-    #END DEBUG
+    @name     = values[:name] or raise RuntimeError, "no name provided for WebSite: #{values.inspect}"
+    @key      = values[:key]
+    @key      = @name if @key.nil? || @key == ''
     @username = values[:username] or raise RuntimeError, "no username provided for WebSite: #{values.inspect}"
     @password = values[:password] or raise RuntimeError, "no password provided for WebSite: #{values.inspect}"
     @url      = values[:url]      || ''
     @notes    = values[:notes]    || ''
+
   end
   
   def to_s
@@ -51,6 +50,10 @@ class SafeEntry
   end
 end
 
+
+### ----------------------------- ###
+### ---[ Class: PasswordSafe ]--- ###
+### ----------------------------- ###
 
 class PasswordSafe
 
@@ -66,6 +69,9 @@ class PasswordSafe
     @ary_entries = []    # in memory version of entries: ary of SafeEntry_s
     @crypto = GPGME::Crypto.new(:armor => true)
     read_safe
+    @ary_entries.sort_by! do |e|
+      e.name
+    end
     self
   end
 
@@ -81,6 +87,12 @@ class PasswordSafe
   def add_entry(e)
     @ary_entries << SafeEntry.new(e)
   end
+
+  def sort
+    @ary_entries.sort_by! do |e|
+      e.name
+    end    
+  end
   
   # Predicate specifying whether the password safe has been
   # provided a key to be opened.  If not, the open method must
@@ -92,7 +104,38 @@ class PasswordSafe
     not @ary_entries.nil?
   end
 
+  # xxxxxxxxxxxxxxxxxx
+  # 
+  # @param [Number] num size
+  # @return [Array<String>] returns xxx
+  def update_entry(oldent, newent)
+    @ary_entries[ @ary_entries.index(oldent) ] = newent
+    self.sort
+  end
+  
+
+  # Gets all entries matching or partially matching the key passed in.
+  # The key can have "glob" type matching wildcards (e.g., *bank*)
+  # which get transformed into regex notation (/.*bank.*/). The match
+  # will also ignore case.
+  # 
+  # @param [String] key - full or partial match to one or more keys
+  #   in the password safe
+  # @return [Array<SafeEntry>] returns all entries that
+  #   that matched the key or Empty List if no match
   def get_entries_by_key(key)
+    re = Regexp.new(key.gsub(/\*/, '.*'), true)  # true => ignore case
+    @ary_entries.select { |e| re =~ e.key }
+  end
+
+  # All SafeEntry objects passed in will be deleted
+  # from the safe.
+  # 
+  # @param [Number] num size
+  # @return [Array<String>] returns xxx
+  def delete_entries(ary_e)
+    # use array difference operator
+    @ary_entries = @ary_entries - ary_e
   end
 
   # For debugging only
@@ -102,17 +145,25 @@ class PasswordSafe
     str << self.to_s
   end
 
-  def to_s
-    @ary_entries.map { |e| e.to_s }.join( "\n" + ('-' * 50) + "\n")
+  # Returns all entries as a string with "--------" line delimeter
+  # between them.  The strings are created by calling to_s on each entry.
+  # 
+  # @return [String] returns String representation of all entries in the safe
+  def to_s(entries = @ary_entries)
+    entries.map { |e| e.to_s }.join( "\n" + ('-' * 50) + "\n")
   end
-  
+
+  # xxxxxxx
+  # 
+  # 
+  # @return [void]
   def read_safe
     return if not File.exist? @pwsafe
     cipher = GPGME::Data.new( IO.read(@pwsafe) )
     plaintxt = @crypto.decrypt(cipher, {:password => @password}).read
 
     entry_info = Hash.new
-#    File.open(@pwsafe, "r") do |sio|
+    #    File.open(@pwsafe, "r") do |sio|
     StringIO.open(plaintxt, "r") do |sio|
       while str = sio.gets
         (key, val) = str.chomp.split(/\s*:\s*/)
@@ -132,8 +183,8 @@ class PasswordSafe
     cipher = @crypto.encrypt(self.to_s, {
                                :symmetric => true,
                                :password => @password,
-#                               :protocol => 4,
-#                               :file => @pwsafe        #~TODO: this part is not working ...
+                               #                               :protocol => 4,
+                               #                               :file => @pwsafe        #~TODO: this part is not working ...
                              })
     # overwrite safe with write encrypted contents
     File.open(@pwsafe, "wb") do |fw|
@@ -142,6 +193,12 @@ class PasswordSafe
   end
   
 end
+
+
+### ----------------------------------------- ###
+### ---[ START "Main" Procedural Section ]--- ###
+### ----------------------------------------- ###
+
 
 def display_options
   puts "Options available..."
@@ -156,7 +213,7 @@ def display_options
   puts " q:    quit"
   print "Enter option: "
 end
-  
+
 def get_user_choice
   while true
     e = gets
@@ -167,23 +224,43 @@ def get_user_choice
 end
 
 def password_prompt(msg = "Enter safe password: ", b_prompt_once = true)
-  print msg
-  first = gets
-  unless b_prompt_once
-    print "Enter password again: "
-    second = gets
-    if second != first
-      puts "Sorry passwords do not match. Try again."
-      first = password_prompt(msg, b_prompt_once) 
+  first = ''
+  $stdout.write msg
+  $stdout.flush
+  begin
+    system('stty -echo')
+    io = IO.for_fd(0, 'w')  # file desc 0 == STDIN
+    first = gets.chomp
+    io.puts("")
+    io.flush
+    
+    unless b_prompt_once
+      print "Enter password again: "
+      second = gets.chomp
+      if second != first
+        puts "\nSorry passwords do not match. Try again."
+        first = password_prompt(msg, b_prompt_once) 
+      end
+      io.flush
     end
+
+  ensure
+    (0 ... $_.length).each do |i| $_[i] = ?0 end if $_
+    system('stty echo')
   end
+
   first
 end
 
 def add_new_entry(safe)
   # TODO: need to allow a user to abort or start over ...
+  h = prompt_for_all_safe_entry_fields
+  safe.add_entry(h)
+end
+
+def prompt_for_all_safe_entry_fields
   h = Hash.new
-  print "Enter Name of new entry: "
+  print "Enter Name: "
   h[:name] = gets.chomp
   print "Key for lookups: "
   h[:key] = gets.chomp
@@ -195,16 +272,73 @@ def add_new_entry(safe)
   h[:url] = gets.chomp
   print "Notes: "
   h[:notes] = gets.chomp
-  safe.add_entry(h)
+  h
+end
+
+def user_lookup_prompt(safe)
+  print "Enter lookup key ('*' for wildcards): "
+  safe.get_entries_by_key(gets.chomp)
 end
 
 def delete_entry(safe)
+  ary = user_lookup_prompt(safe)
+  if ary.empty?
+    puts "No match found"
+  else
+    puts "This will delete the following entries: "
+    ary.each { |e| puts "  #{e.name}" }
+    while true
+      print "Proceed? (Y/n): "
+      sel = gets.chomp
+      case sel
+      when '', 'y', 'Y'; return safe.delete_entries(ary)
+      when 'n', 'N'    ; return 
+      end
+    end  
+  end
 end
 
 def update_entry(safe)
+  ary = user_lookup_prompt(safe)
+  if ary.size == 0
+    puts "No entry found"
+  elsif ary.size > 1
+    puts "More than one entry matched: "
+    ary.each {|e| puts "  #{e}"}
+    puts "Please refine search"
+  else
+    # size == 1, proceed to get new info
+    puts "Matched:\n#{ary.first.to_s}"
+    while true
+      print "Edit password (p) or all fields (a)? (P/a): "
+      sel = gets.chomp
+      case sel
+      when '', 'p', 'P'
+        sel = :password
+        break
+      when 'a', 'A'
+        sel = :all
+        break
+      else
+        puts "Input '#{sel}' not recognized."
+      end
+    end  
+
+    if sel == :password
+      replace = ary.first.dup
+      print "Enter new password: " 
+      replace.password = gets.chomp
+    else
+      h = prompt_for_all_safe_entry_fields
+      replace = SafeEntry.new(h)
+    end
+    safe.update_entry(ary.first, replace)
+  end
 end
 
 def get_entry(safe)
+  r = safe.to_s( user_lookup_prompt(safe) )
+  puts (r != '' ? r : "No match found")
 end
 
 def list_all_entries(safe)
@@ -241,7 +375,7 @@ def open_safe(safe)
   password = ''
   if not File.exist? PASSWORD_SAFE_FILE
     puts "Password safe file does not exist."
-    password = password_prompt("Enter password to create new safe (q to quit): ", false)
+    password = password_prompt("Enter password to create new safe: ", false)
   else
     password = password_prompt
   end
